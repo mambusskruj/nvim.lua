b4cecfb4bc7a16e507265545c0c0e9a5232c6478
Dmytro Domin <mambus.skruj@gmail.com>
2024-01-20 13:56:56 +0100

    copilot changes
    - allow copilot to work within commit message, idk why it doesn't work
      in neogit filetype

--- lua/config/plugins/copilot.lua
+++ lua/config/plugins/copilot.lua
@@ -1,19 +1,24 @@
 return {
-    {
-        "zbirenbaum/copilot.lua",
-        opts = {
-            suggestion = {
-                enabled = true,
-                auto_trigger = false,
-                keymap = {
-                    accept = "<leader>j",
-                    accept_word = false,
-                    accept_line = false,
-                    next = "<leader>d",
-                    prev = "<leader>s",
-                    dismiss = "<leader>a",
-                }
-            }
-        }
-    }
+	{
+		"zbirenbaum/copilot.lua",
+		lazy = false,
+		opts = {
+			filetypes = {
+				gitcommit = true,
+				NeogitCommitMessage = true,
+			},
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				keymap = {
+					accept = "<leader>j",
+					accept_word = false,
+					accept_line = false,
+					next = "<leader>d",
+					prev = "<leader>s",
+					dismiss = "<leader>a",
+				},
+			},
+		},
+	},
 }
