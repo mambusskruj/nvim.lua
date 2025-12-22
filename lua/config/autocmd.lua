5ca597245527d53d68980dedc6ab802289a43e7e
Dmytro Domin <mambus.skruj@gmail.com>
2025-12-22 07:27:07 +0100

    keymaps for lsp

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -39,6 +39,40 @@ vim.api.nvim_create_autocmd("LspAttach", {
 	end,
 })
 
+-- Keymaps for LSP clients
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(event)
+		local bufmap = function(mode, rhs, lhs)
+			vim.keymap.set(mode, rhs, lhs, { buffer = event.buf })
+		end
+
+		-- These keymaps are the defaults in Neovim v0.11
+		-- bufmap("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")
+		-- bufmap("n", "grr", "<cmd>lua vim.lsp.buf.references()<cr>")
+		-- bufmap("n", "gri", "<cmd>lua vim.lsp.buf.implementation()<cr>")
+		-- bufmap("n", "grn", "<cmd>lua vim.lsp.buf.rename()<cr>")
+		-- bufmap("n", "gra", "<cmd>lua vim.lsp.buf.code_action()<cr>")
+		-- bufmap("n", "gO", "<cmd>lua vim.lsp.buf.document_symbol()<cr>")
+		-- bufmap({ "i", "s" }, "<C-s>", "<cmd>lua vim.lsp.buf.signature_help()<cr>")
+
+		-- These are custom keymaps
+		-- bufmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>")
+		-- bufmap("n", "grt", "<cmd>lua vim.lsp.buf.type_definition()<cr>")
+		-- bufmap("n", "grd", "<cmd>lua vim.lsp.buf.declaration()<cr>")
+		-- bufmap({ "n", "x" }, "gq", "<cmd>lua vim.lsp.buf.format({async = true})<cr>")
+
+		bufmap("n", "gR", "<cmd>lua vim.lsp.buf.rename()<CR>")
+		bufmap("n", "gA", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+		bufmap("n", "gF", "<cmd>lua vim.lsp.buf.format({async = true})<CR>")
+
+		bufmap("n", "gl", "<cmd>lua vim.diagnostic.setloclist()<CR>")
+		bufmap("n", "gp", "<cmd>lua vim.diagnostic.setqflist()<CR>")
+		bufmap("n", "g]", "<cmd>lua vim.diagnostic.goto_next()<CR>")
+		bufmap("n", "g[", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
+		bufmap("n", "gh", "<cmd>lua vim.diagnostic.open_float()<CR>")
+	end,
+})
+
 -- Automatically load launch.json for debugging
 -- I need it because I provide custom path to launch.json, so it do not reload file automatically
 vim.api.nvim_create_autocmd("BufWritePost", {
